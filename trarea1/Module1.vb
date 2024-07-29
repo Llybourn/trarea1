cf85bb306eeca93b1c2b344e2da314d7c53e2b59
LENOVO <LENOVO@DELL>
2024-07-28 22:34:10 -0600

    Agregar archivos de proyecto.

---
+++ trarea1/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mensaje, nombre As String
+        Console.Clear()
+        Console.WriteLine("Ingrese nombre y #carnet")
+        nombre = Console.ReadLine
+        Console.Clear()
+        mensaje = "Hola mundo "
+        Console.WriteLine(mensaje + nombre + "2024")
+        Console.WriteLine("Presione cualquier tecla para salir")
+        Console.ReadKey()
+    End Sub
+
+End Module
